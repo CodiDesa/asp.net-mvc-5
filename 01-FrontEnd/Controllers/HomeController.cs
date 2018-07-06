487526d322fe08645607565cdb78267892a9e9ea
Juan Acosta <33112051+CodiDesa@users.noreply.github.com>
2018-07-06 12:31:19 -0500

    Versión 1.0 Inicial
    Versión de inicio de la app

---
+++ 01-FrontEnd/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Common;
+using NLog;
+using Service;
+using System.Web.Mvc;
+
+namespace FrontEnd.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger _logger = LogManager.GetCurrentClassLogger();
+        private readonly ITestService _testService = DependecyFactory.GetInstance<ItestService>();
+        
+        public ActionResult Index()
+        {
+            _logger.Info("Comenzo el método Index");
+            var value = Parameters.TaxValue;
+            var courses = _testService.getall();
+
+            _logger.Info("Obtuvo un parametro de app parametrs. config");
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ 02-Service/Service/TestService.cs
@@ -0,0 +1,41 @@
+﻿using Model.Domain;
+using Persistence.DbContextScope;
+using Persistence.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public interface ItestService
+    {
+        IEnumerable<Course> GetAll();
+    }
+
+    public class TestService : ItestService
+    {
+        private readonly IDbContextScopeFactory _dbContextScopeFactory;
+        private readonly IRepository<Course> _courseRepo;
+        public TestService(
+            IDbContextScopeFactory dbContextScopeFactory,
+            IRepository<Course> courseRepo
+
+            )
+        {
+            _dbContextScopeFactory = dbContextScopeFactory;
+            _courseRepo = courseRepo;
+        }
+       
+        public IEnumerable<Course> GetAll()
+        {
+            var courses = new List<Course>();
+            using (var ctx = _dbContextScopeFactory.CreateReadOnly())
+            {
+                courses = _courseRepo.GetAll(x=>x.StudentPerCourses).ToList();
+            }
+                return courses;
+        }
+    }
+}
